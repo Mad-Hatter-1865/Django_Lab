0e4525801bc4b3a5ec310be22a83c0274b38dbfc
Michael Martinez <rusalka2100@gmail.com>
2019-09-09 16:27:25 -0500

    Part 1 Complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+class Game:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, title, platform, description, relyear):
+    self.title = title
+    self.platform = platform
+    self.description = description
+    self.relyear = relyear
+
+games = [
+  Game('Fallout: New Vegas', 'PC', 'Fantastic', 2010),
+  Game('Deus Ex: Human Revolution', 'PC', 'Fantastic', 2011),
+  Game('Witcher 3: Wild Hunt', 'PC', 'Great', 2015)
+]
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def games_index(request):
+    return render(request, 'games/index.html', {'games': games})
+
+
